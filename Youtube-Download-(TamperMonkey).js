116e7480339fce70e3da911993cff09cea153897
Zetat <zetaec27@gmail.com>
2017-02-10 07:53:22 -0200

    Add files via upload

---
+++ Youtube-Download-(TamperMonkey).js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Youtube Download
+// @version      1.0
+// @description  Baixar áudio e vídeo do youtube
+// @author       Zeta Tec
+// @match        *://www.youtube.com/*
+// @exclude         *://www.youtube.com/tv*
+// @exclude         *://www.youtube.com/embed/*
+// @exclude         *://www.youtube.com/live_chat*
+// @icon            http://zetatec27.blogspot.com.br/favicon.ico
+// @encoding utf-8
+// @updateURL https://sites.google.com/site/geradorzeta/zetatec/Youtube-Download-(TamperMonkey).js
+// @downloadURL https://sites.google.com/site/geradorzeta/zetatec/Youtube-Download-(TamperMonkey).js
+// ==/UserScript==
+(function() {
+
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "";
+window.onload = document.head.appendChild(script);
+
+
+})();
