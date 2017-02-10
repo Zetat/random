81bc70b239d5bd10cfa830c377767a6df221b89f
Zetat <zetaec27@gmail.com>
2017-02-10 08:01:56 -0200

    Update Youtube-Download-(TamperMonkey).user.js

--- Youtube-Download-(TamperMonkey).user.js
+++ Youtube-Download-(TamperMonkey).user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Youtube Download
-// @version      1.0
+// @version      1.1
 // @description  Baixar áudio e vídeo do youtube
 // @author       Zeta Tec
 // @match        *://www.youtube.com/*
@@ -9,14 +9,14 @@
 // @exclude         *://www.youtube.com/live_chat*
 // @icon            http://zetatec27.blogspot.com.br/favicon.ico
 // @encoding utf-8
-// @updateURL https://cdn.rawgit.com/Zetat/random/master/Youtube-Download-(TamperMonkey).user.js
-// @downloadURL https://cdn.rawgit.com/Zetat/random/master/Youtube-Download-(TamperMonkey).user.js
+// @updateURL https://cdn.rawgit.com/Zetat/random/master/Youtube-Download-(TamperMonkey).js
+// @downloadURL https://cdn.rawgit.com/Zetat/random/master/Youtube-Download-(TamperMonkey).js
 // ==/UserScript==
 (function() {
 
 var script = document.createElement("script");
 script.type = "text/javascript";
-script.src = "";
+script.src = "https://cdn.rawgit.com/Zetat/random/master/source.js";
 window.onload = document.head.appendChild(script);
 
 
